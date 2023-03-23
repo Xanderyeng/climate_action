2c192ed7c0b62cd36792e22b90c22d10431b1499
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-03-23 15:04:17 +0300

    Landing Page - Not Responsive

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,20 @@
 import coat_of_arms from "./Coat_of_arms_of_Kenya.png";
 import au_icon from "./au_icon.svg";
 import windvanes from "./windvanes.jpg";
-import world_map from "./world-map-africa.svg";
+// import world_map from "./world-map-africa.svg";
 import trapeziums from "./trapeziums.svg";
 import the_president from "./president-ruto.png";
 import backdrop from './custom-bg.png'
 import left from "./left.svg"
 import right from "./right.svg"
-import participate from './participate_trimmed.png'
 
 export {
   coat_of_arms,
   au_icon,
   windvanes,
-  world_map,
   trapeziums,
   the_president,
   backdrop,
   left,
-  right,
-  participate
+  right
 };
